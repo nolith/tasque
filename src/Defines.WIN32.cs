a08fbc3e727a9d91084738c065fe190080da9d9b
Sandy Armstrong <sanfordarmstrong@gmail.com>
2008-09-26 10:57:01 +0000

    Rename Defines.cs for Windows.
    2008-09-26  Sandy Armstrong <sanfordarmstrong@gmail.com> 
    
    	* tasque.csproj, src/Defines.WIN32.cs: Rename Defines.cs for Windows.
    
    svn path=/trunk/; revision=111

--- src/Defines.WIN32.cs
+++ src/Defines.WIN32.cs
@@ -33,8 +33,8 @@ namespace Tasque
 	public class Defines
 	{
 		public const string Version		= "0.1.8";
-		public const string DataDir		= "/usr/share";
-		public const string LocaleDir	= "/usr/share/locale";
-		public const string SoundDir		= "/usr/share/tasque/sounds";
+		public const string DataDir		= "@datadir@";
+		public const string LocaleDir	= "@datadir@/locale";
+		public const string SoundDir		= "@datadir@/tasque/sounds";
 	}
 }
